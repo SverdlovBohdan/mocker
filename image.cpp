20ebe46a06c2378dd07bbf75d447fcb601375895
bsverdlov <freezar92@gmail.com>
2023-06-10 14:47:10 +0300

    Minor Image class improvements

--- image.cpp
+++ image.cpp
@@ -24,6 +24,12 @@ Image::~Image() {
         std::move(image_reading_task_handle_));
     image_reading_task_handle_ = TaskHandle{nullptr};
   }
+
+  if (image_texture_id_) {
+    GLuint texture_id = image_texture_id_.value();
+    image_texture_id_.reset();
+    glDeleteTextures(1, &texture_id);
+  }
 }
 
 void Image::Display() {
@@ -39,63 +45,15 @@ void Image::Display() {
 
     case ReadyStatus::kNone:
       status_ = ReadyStatus::kReading;
-      image_reading_task_handle_ = filesystem_task_dispatcher_->PostTask(
-          [lifetime_controller = shared_from_this()]() {
-            // Filesystem thread.
-            auto texture = lifetime_controller->LoadImageDataFromFile(
-                lifetime_controller->image_path_);
-
-            if (texture.has_value()) {
-              lifetime_controller->ui_task_dispatcher_->PostTask(
-                  [texture_image = std::move(texture.value()),
-                   lifetime_controller]() {
-                    // UI thread.
-                    lifetime_controller->OnTextureReadingSuccess(
-                        std::move(texture_image));
-                  });
-            } else {
-              lifetime_controller->ui_task_dispatcher_->PostTask(
-                  [lifetime_controller]() {
-                    // UI thread.
-                    lifetime_controller->OnError();
-                  });
-            }
-          });
+      image_reading_task_handle_ = LoadImageFromFileOnFilesystemThread();
       break;
 
     case ReadyStatus::kReady:
       if (!image_texture_id_) {
-        GLenum format = GL_RGB;
-        // TODO(BoSv): add more formats.
-        if (texture_.channels == 4) {
-          format = GL_RGBA;
-        }
-        // Create a OpenGL texture identifier
-        intptr_t image_texture;
-        glGenTextures(1, reinterpret_cast<GLuint*>(&image_texture));
-        glBindTexture(GL_TEXTURE_2D, image_texture);
-
-        // Setup filtering parameters for display
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
-                        GL_CLAMP_TO_EDGE);  // This is required on WebGL for non
-                                            // power-of-two textures
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,
-                        GL_CLAMP_TO_EDGE);  // Same
-
-        // Upload pixels into texture
-#if defined(GL_UNPACK_ROW_LENGTH) && !defined(__EMSCRIPTEN__)
-        glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
-#endif
-        glTexImage2D(GL_TEXTURE_2D, 0, static_cast<GLint>(format),
-                     texture_.width, texture_.height, 0, format,
-                     GL_UNSIGNED_BYTE, texture_.image_data.data());
-        image_texture_id_ = image_texture;
+        image_texture_id_ = GenerateImageOpenGlTexture();
       }
 
       if (image_texture_id_) {
-        std::cout << "Draw image" << std::endl;
         ImGui::Image(reinterpret_cast<void*>(*image_texture_id_),
                      ImVec2(texture_.width, texture_.height));
       }
@@ -106,8 +64,51 @@ void Image::Display() {
   }
 }
 
+TaskHandle Image::LoadImageFromFileOnFilesystemThread() {
+  return filesystem_task_dispatcher_->PostTask([lifetime_controller =
+                                                    shared_from_this()]() {
+    // Filesystem thread.
+    auto texture = lifetime_controller->LoadImageDataFromFile(
+        lifetime_controller->image_path_);
+
+    if (texture.has_value()) {
+      lifetime_controller->ui_task_dispatcher_->PostTask(
+          [texture_image = std::move(texture.value()), lifetime_controller]() {
+            // UI thread.
+            lifetime_controller->OnTextureReadingSuccess(
+                std::move(texture_image));
+          });
+    } else {
+      lifetime_controller->ui_task_dispatcher_->PostTask(
+          [lifetime_controller]() {
+            // UI thread.
+            lifetime_controller->OnError();
+          });
+    }
+  });
+}
+
 tl::expected<Image::ImageTexture, std::error_code> Image::LoadImageDataFromFile(
     std::filesystem::path image_path) {
+  //  TODO(BoSv): Adapt image resizing.
+  // if (!IsPowerOfTwo(width) || !IsPowerOfTwo(height)) {
+  // 	//std::cout << "size is not power of two! resizing... ";
+  // 	std::vector<int> powers = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024,
+  // 2048}; 	int new_size = std::max(closest(powers, width), closest(powers,
+  // height));
+  // 	//std::cout << "new_size: " << new_size << "x" << new_size << " ";
+  // 	unsigned char* data_resized = (unsigned char*) malloc(new_size *
+  // new_size * nrComponents); 	stbir_resize_uint8(data, width, height, 0,
+  // data_resized, new_size, new_size, 0, nrComponents);
+  // 	//std::cout << "resized! ";
+
+  // 	stbi_image_free(data);
+  // 	data = data_resized;
+  // 	width = new_size;
+  // 	height = new_size;
+  // 	resolution = new_size;
+  // }
+
   int x = 0;
   int y = 0;
   int channels = 0;
@@ -129,6 +130,37 @@ tl::expected<Image::ImageTexture, std::error_code> Image::LoadImageDataFromFile(
   stbi_image_free(image_data);
 
   return texture;
+}
+
+intptr_t Image::GenerateImageOpenGlTexture() {
+  GLenum format = GL_RGB;
+  // TODO(BoSv): add more formats.
+  if (texture_.channels == 4) {
+    format = GL_RGBA;
+  }
+  // Create a OpenGL texture identifier
+  intptr_t image_texture;
+  glGenTextures(1, reinterpret_cast<GLuint*>(&image_texture));
+  glBindTexture(GL_TEXTURE_2D, image_texture);
+
+  // Setup filtering parameters for display
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
+                  GL_CLAMP_TO_EDGE);  // This is required on WebGL for non
+                                      // power-of-two textures
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,
+                  GL_CLAMP_TO_EDGE);  // Same
+
+  // Upload pixels into texture
+#if defined(GL_UNPACK_ROW_LENGTH) && !defined(__EMSCRIPTEN__)
+  glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+#endif
+  glTexImage2D(GL_TEXTURE_2D, 0, static_cast<GLint>(format), texture_.width,
+               texture_.height, 0, format, GL_UNSIGNED_BYTE,
+               texture_.image_data.data());
+
+  return image_texture;
 }
 
 void Image::OnTextureReadingSuccess(ImageTexture image_texture) {
--- image.h
+++ image.h
@@ -4,8 +4,8 @@
 
 #include <cstddef>
 #include <memory>
-#include <system_error>
 #include <optional>
+#include <system_error>
 #include <tl/expected.hpp>
 #include <vector>
 
@@ -27,13 +27,21 @@ class Image : public ImageView, public std::enable_shared_from_this<Image> {
   void Display() override;
 
  private:
+  /**
+   * @brief Image ready status.
+   *
+   */
   enum class ReadyStatus {
-    kNone,
-    kReading,
-    kError,
-    kReady,
+    kNone,     ///< Initialialization state.
+    kReading,  ///< Image data is reading from file.
+    kError,    ///< Image reading has failed.
+    kReady,    ///< Image is ready to display.
   };
 
+  /**
+   * @brief Image texture context.
+   *
+   */
   struct ImageTexture {
     std::vector<std::byte> image_data;
     std::size_t width{0};
@@ -41,9 +49,30 @@ class Image : public ImageView, public std::enable_shared_from_this<Image> {
     std::size_t channels{0};
   };
 
+  /**
+   * @brief Load image data from file on filesystem thread.
+   *
+   * @return TaskHandle Filesystem thread task handle.
+   */
+  TaskHandle LoadImageFromFileOnFilesystemThread();
+
+  /**
+   * @brief Load image data from file.
+   *
+   * @param image_path Absoluth path to file.
+   * @return ImageTexture in success. Otherwise error code.
+   */
   tl::expected<ImageTexture, std::error_code> LoadImageDataFromFile(
       std::filesystem::path image_path);
 
+  /**
+   * @brief Generate texture and push image data to GPU.
+   *
+   * @return Texture id.
+   */
+  intptr_t GenerateImageOpenGlTexture();
+
+  // Handlers in UI thread.
   void OnTextureReadingSuccess(ImageTexture image_texture);
   void OnError();
 
@@ -53,8 +82,8 @@ class Image : public ImageView, public std::enable_shared_from_this<Image> {
 
   ReadyStatus status_;
   TaskHandle image_reading_task_handle_;
-  ImageTexture texture_;
 
+  ImageTexture texture_;
   std::optional<intptr_t> image_texture_id_;
 };
 }  // namespace mk
